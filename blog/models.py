59849e6310094527e8fc98bf915116cab01f9ddd
Pallavi Mohindru <pallavi.mohindru@publicissapient.com>
2020-02-26 13:45:24 +0530

    blog model and admin

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class blog(models.Model):
+    title = models.CharField(max_length= 20)
+    publish_date = models.DateField()
+    image = models.ImageField(upload_to = 'images/')
+    body = models.CharField(max_length= 20)
